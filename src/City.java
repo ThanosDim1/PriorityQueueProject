1129b3d001e4f21cbc132ed382f708e18808a104
Thanasis Dimitrakopoulos <git@github.com>
2023-12-05 10:48:44 +0200

    Creation of the City object and the main class

--- src/City.java
+++ src/City.java
@@ -1,42 +1,52 @@
 public class City implements CityInterface{
+    int ID;
+    String Name;
+    int Population;
+    int InfCases;
+    public City(int ID,String Name,int Population,int InfCases){
+        this.ID=ID;
+        this.Name=Name;
+        this.Population=Population;
+        this.InfCases=InfCases;
+    }
 
     @Override
     public int getID() {
-        return 0;
+        return this.ID;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.Name;
     }
 
     @Override
     public int getPopulation() {
-        return 0;
+        return this.Population;
     }
 
     @Override
     public int getInfluenzaCases() {
-        return 0;
+        return this.InfCases;
     }
 
     @Override
     public void setID(int ID) {
-
+        this.ID=ID;
     }
 
     @Override
     public void setName(String name) {
-
+        this.Name=Name;
     }
 
     @Override
     public void setPopulation(int population) {
-
+        this.Population=population;
     }
 
     @Override
     public void setInfluenzaCases(int InfluenzaCases) {
-
+        this.InfCases=InfluenzaCases;
     }
 }
