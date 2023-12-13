ab8d203f71ad796ea30f36c7845eb1cf49f76b1a
Thanasis Dimitrakopoulos <git@github.com>
2023-12-13 19:17:57 +0200

    Roufa

--- src/DynamicInfluenza_k_withPQ.java
+++ src/DynamicInfluenza_k_withPQ.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 
-public class Influenza_k_withPQ {
-    public Influenza_k_withPQ(Scanner scanner) {
+public class DynamicInfluenza_k_withPQ {
+    public DynamicInfluenza_k_withPQ(Scanner scanner) {
         // Ask the user for the file name
         System.out.println("Enter the whole path of the text file:");
         String fileName = scanner.nextLine();
-        
+
         // Create an instance of ReadFile to read cities from the file
         ReadFile readFile = new ReadFile(fileName);
-        
-        
-        // Get the array of cities from ReadFile
-        City[] cities = readFile.getCities();
 
         // Create an instance of PQ
         PQ priorityQueue = new PQ();
@@ -22,23 +18,27 @@ public class Influenza_k_withPQ {
             System.out.println("The number you entered exceeds the number of cities");
             return;
         }
-        
 
-        // Insert each city into the priority queue
-        for (City city : cities) {
-            priorityQueue.insert(city);
+        // Read cities line by line and insert them into the priority queue
+        for (City city : readFile.getCities()) {
+            city.CalculateDensity(); // Calculate density for the city
+            priorityQueue.insert(city); // Insert the city into the priority queue
 
             // Ensure that the priority queue has at most k elements
             while (priorityQueue.size() > k) {
                 priorityQueue.getmin();
             }
-        }
 
-        
+        }
+        // Print the top k cities with the least density
+        System.out.println("Top " + k + " cities with the least density:");
+        for (int i = 0; i < k; i++) {
+            System.out.println(priorityQueue.min().getName());
+        }
     }
+
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            Influenza_k_withPQ mainObj = new Influenza_k_withPQ(scanner);
+        Scanner scanner = new Scanner(System.in);
+        DynamicInfluenza_k_withPQ mainObj = new DynamicInfluenza_k_withPQ (scanner);
     }
 }
-
